677728fe27d9c1e60e0f7af76d02871f2a77ce27
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-26 17:45:37 +0100

    Add logic to change grid size and display it

--- script.js
+++ script.js
@@ -16,9 +16,32 @@ for (i=0;i<defaultSide*defaultSide;i++){
 
 function generateGrid () {
     container.innerHTML="";
+
+    let side;
+
+    do {
+        side = parseInt(prompt("Insert the grid size (as a single number representing a side less than 100"),10);
+    }while(isNaN(side) || side<=0 || side>100);
+
+    let sizeDisplay = document.querySelectorAll(".size");
+    sizeDisplay.forEach(element => {element.textContent=side})
     
+    for (i=0;i<side*side;i++){
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("gridElement");
+        gridElement.style.width = 600/side + "px"
+        gridElement.style.height = 600/side + "px"
+        container.appendChild(gridElement);
+    }
+    
+    const gridElements = document.querySelectorAll(".gridElement");
+
+    gridElements.forEach(element => {
+        element.addEventListener("mouseenter", () => element.style.backgroundColor="black");
+    });
 
 }
+
 
 
 const gridElements = document.querySelectorAll(".gridElement");
