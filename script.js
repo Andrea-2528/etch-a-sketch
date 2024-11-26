6f3fb85359dd92d8bfade6f0517738059a9e0af9
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-26 19:02:14 +0100

    Add random rgb value and incrementing opacity

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const container = document.querySelector(".container");
 const buttonGridChange = document.querySelector("button");
 
-buttonGridChange.addEventListener("click", generateGrid)
+buttonGridChange.addEventListener("click", generateGrid);
 
 
-let defaultSide=16;
+let defaultSide=32;
 
 for (i=0;i<defaultSide*defaultSide;i++){
     const gridElement = document.createElement("div");
     gridElement.classList.add("gridElement");
-    gridElement.style.width = 600/defaultSide + "px"
-    gridElement.style.height = 600/defaultSide + "px"
+    gridElement.style.backgroundColor = "rgb(255, 255, 255)";
+    gridElement.style.opacity = "0.1";
+    gridElement.style.width = 600/defaultSide + "px";
+    gridElement.style.height = 600/defaultSide + "px";
     container.appendChild(gridElement);
 }
 
@@ -29,17 +31,34 @@ function generateGrid () {
     for (i=0;i<side*side;i++){
         const gridElement = document.createElement("div");
         gridElement.classList.add("gridElement");
-        gridElement.style.width = 600/side + "px"
-        gridElement.style.height = 600/side + "px"
+        gridElement.style.backgroundColor = "rgb(255, 255, 255)";
+        gridElement.style.opacity = "0.1";
+        gridElement.style.width = 600/side + "px";
+        gridElement.style.height = 600/side + "px";
         container.appendChild(gridElement);
     }
     
     const gridElements = document.querySelectorAll(".gridElement");
 
     gridElements.forEach(element => {
-        element.addEventListener("mouseenter", () => element.style.backgroundColor="black");
+        element.addEventListener("mouseenter", () => {
+            if (element.style.backgroundColor == "rgb(255, 255, 255)") {
+                element.style.backgroundColor = getRandomColor();
+                element.style.opacity = "0.1";
+            }else {
+                let currentOpacity = parseFloat(element.style.opacity);
+                element.style.opacity = currentOpacity + 0.1;
+            }
+        });
     });
 
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 
@@ -47,5 +66,13 @@ function generateGrid () {
 const gridElements = document.querySelectorAll(".gridElement");
 
 gridElements.forEach(element => {
-    element.addEventListener("mouseenter", () => element.style.backgroundColor="black");
+    element.addEventListener("mouseenter", () => {
+        if (element.style.backgroundColor == "rgb(255, 255, 255)") {
+            element.style.backgroundColor = getRandomColor();
+            element.style.opacity = "0.1";
+        }else {
+            let currentOpacity = parseFloat(element.style.opacity);
+            element.style.opacity = currentOpacity + 0.1;
+        }
+    });
 });
